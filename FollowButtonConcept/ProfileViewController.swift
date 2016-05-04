5039da215f36ff1de93f2fabb312eff89470234f
Louis Tur <louis.tur@gmail.com>
2016-05-03 21:12:19 -0400

    STABLE: initial commit

---
+++ FollowButtonConcept/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FollowButtonConcept
+//
+//  Created by Louis Tur on 5/3/16.
+//  Copyright © 2016 SRLabs. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    
+  }
+
+
+}
+
